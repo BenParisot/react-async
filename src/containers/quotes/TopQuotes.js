05004d6e57e237cf4fdd22d0e2dd2517a49bda7c
Ben Parisot <me@benparisot.com>
2019-05-16 13:42:44 -0700

    getquote service works

--- src/containers/quotes/TopQuotes.js
+++ src/containers/quotes/TopQuotes.js
@@ -1,2 +1,19 @@
 import React, { PureComponent } from 'react';
 import Quotes from '../../components/Quotes';
+import GetQuote, { getQuotes } from '../../services/getQuote';
+
+export default class TopQuotes extends PureComponent {
+    state = {
+        quotes: []
+    }
+
+    componentDidMount() {
+        getQuotes()
+        .then(quotes => this.setState({ quotes }));
+    }
+
+    render() {
+        const { quotes } = this.state;
+        return <Quotes quotes={quotes} />
+    }
+}
