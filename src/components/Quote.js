84427fad146176559e4bc2cd0d4d75a73823f10b
Ben Parisot <me@benparisot.com>
2019-05-16 13:23:19 -0700

    write Quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Styles from '../main.css';
+
+export default function Quote({ character, quote, image }) {
+  return (
+    <section className={Styles.quote}>
+      <li>{character}</li>
+      <li>{quote}</li>
+      <li>{image}</li>
+    </section>
+  );
+}
+Quote.propTypes = {
+  character: PropTypes.string.isRequired,
+  quote: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired
+};
